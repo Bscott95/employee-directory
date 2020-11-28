8935344eeb215adba48aa49de4dc9179f408b4b3
Brandon Scott <btfs95@gmail.com>
2020-11-28 12:25:35 -0800

    got mdbreact to work - imported 3 css files to the top of Grid.js

--- src/Components/Grid.jsx
+++ src/Components/Grid.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { MDBDataTable } from 'mdbreact';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
 
 export const Grid = () => {
   const data = {
