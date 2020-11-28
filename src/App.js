9e0bf68860a4abc64e757cf4d256a007e9517dbf
Brandon Scott <btfs95@gmail.com>
2020-11-28 11:43:55 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Navbar } from './Components/Navbar'
+import { Grid } from './Components/Grid'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Grid/>
+    </div>
+  );
+}
+
+export default App;
